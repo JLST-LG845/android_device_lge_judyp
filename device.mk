441ac341553a5c59e56a2edf3a5576c952fc1321
Tuan Anh <tuan73176@gmail.com>
2024-02-10 22:17:27 +0700

    [DNM] judyp: Add shim for ICU
    * Inspired by SamarV-121/android_device_google_sprout@e29dfad02df1b9bf168dceb3be0e289408c735e0
      and adapted for our needs.
    
    * This will solve:
      CANNOT LINK EXECUTABLE "/system/bin/keystore2": cannot locate symbol
      "UCNV_FROM_U_CALLBACK_STOP_68" referenced by "/system/lib64/libandroidicu.so"
    
    Change-Id: Ic6ac603d614585ffa07c118ad2b43eec17e95115

--- device.mk
+++ device.mk
@@ -41,3 +41,7 @@ PRODUCT_PACKAGES += \
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2025-12-31
+
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_icu
