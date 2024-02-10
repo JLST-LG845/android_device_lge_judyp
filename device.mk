0fa219e7832e993741fb32b9ee65f1d1a9eb4b43
Tuan Anh <tuan73176@gmail.com>
2024-02-10 21:31:01 +0700

    judyp: Remove obsolete PRODUCT_STATIC_BOOT_CONTROL_HAL
    * Goolag clearly mentioned it here:
      https://android.googlesource.com/platform/build/+/master/Changes.md#PRODUCT_STATIC_BOOT_CONTROL_HAL
    
    * Remove PRODUCT_STATIC_BOOT_CONTROL_HAL for the sake of it.
    
    Change-Id: Ifeb578fc2ee2081515779ff2518e02c54867cb30

--- device.mk
+++ device.mk
@@ -11,28 +11,21 @@ PRODUCT_PACKAGES += \
     update_verifier \
 #    update_engine_sideload
 
-PRODUCT_PACKAGES += \
-    bootctrl.sdm845
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Enable update engine sideloading by including the static version of the
-# boot_control HAL and its dependencies.
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_PACKAGES += \
     bootctrl.sdm845 \
-    libgptutils \
-    libz \
-    libcutils
-
+    bootctrl.sdm845.recovery
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl.recovery
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
